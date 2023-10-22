b2b0e07f501d204ac00865d035b3d18f5a0d32fc
gitvivek14 <118727613+gitvivek14@users.noreply.github.com>
2023-10-22 11:16:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import NNavbar from "./components/NNavbar";
-import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 // import bg from './assets/images/bg3.png';
 function App() {
